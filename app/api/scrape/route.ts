4c966c942dfcef8f7cbd1ac5baeaf5de2438d3de
Ak_D7 <akshitadeka7@gmail.com>
2025-08-17 23:19:18 +0530

    url API

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest } from "next/server";
+import puppeteer from "puppeteer";
+import { AnalyzeImpl } from "@/utils/analyzeImpl";
 
 const isUrl = (url: string):boolean=>{
 	try {
@@ -10,6 +12,7 @@ const isUrl = (url: string):boolean=>{
 }
 
 export async function POST(request: NextRequest) {
+  let browser;
   try {
     const { url } = await request.json();
     if (!url || !isUrl(url)) {
@@ -19,11 +22,29 @@ export async function POST(request: NextRequest) {
       });
     }
 
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url, { 
+      waitUntil: 'networkidle0',
+      timeout: 30000 
+    });
+
+    const analyzer = new AnalyzeImpl();
+    const report = await analyzer.analyze(page);
+    
+    return new Response(JSON.stringify({ message: report }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
 
   } catch (error) {
     return new Response(JSON.stringify({ error: `${error}` }), {
       status: 400,
       headers: { "Content-Type": "application/json" },
     });
+  }
+  finally {
+    if (browser) await browser.close();
   }
 }
